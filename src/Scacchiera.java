f18724217271a31fdb00fe53bde698335b590562
RodTol <rodolfo.tolloi@gmail.com>
2022-11-08 18:41:28 +0100

    Gradle basic configuration for application

--- src/Scacchiera.java
+++
@@ -1,6 +0,0 @@
-
-public class Scacchiera {
-    public static void main(String[] args) {
-        System.out.println("Gli scacchi sono meglio");
-    }
-}
---
+++ src/main/java/Crossway/start/Main.java
@@ -0,0 +1,11 @@
+package Crossway.start;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello Rodolfo");
+        } else {
+            System.out.println("Hello world");
+        }
+    }
+}
