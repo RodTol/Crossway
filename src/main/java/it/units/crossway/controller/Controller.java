63ee1b5fdd4d5e44ef56cd260f0bdab57c36b9c7
mcorsano <mariannacorsano98@gmail.com>
2022-11-11 00:29:46 +0100

    Controller and GameController modifications

--- src/main/java/it/units/crossway/controller/Controller.java
+++ src/main/java/it/units/crossway/controller/Controller.java
@@ -1,4 +1,6 @@
 package it.units.crossway.controller;
 
+import it.units.crossway.model.Board;
 public interface Controller {
+    public Board getBoard();
 }
--- src/main/java/it/units/crossway/controller/GameController.java
+++ src/main/java/it/units/crossway/controller/GameController.java
@@ -5,8 +5,13 @@ import it.units.crossway.model.Board;
 
 public class GameController implements Controller {
     private Board board;
+    public int p;
 
     public GameController() {
         this.board = new Board(Config.getN_Rows(), Config.getN_Columns());
+    }
+
+    public Board getBoard() {
+        return this.board;
     }
 }
