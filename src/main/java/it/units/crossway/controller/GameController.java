c5b5074e8ac21d161c3fb528f6b0d78e6af05cdb
mcorsano <mariannacorsano98@gmail.com>
2022-11-11 14:44:33 +0100

    Unuseful test field deleted

--- src/main/java/it/units/crossway/controller/GameController.java
+++ src/main/java/it/units/crossway/controller/GameController.java
@@ -5,7 +5,6 @@ import it.units.crossway.model.Board;
 
 public class GameController implements Controller {
     private Board board;
-    public int p;
 
     public GameController() {
         this.board = new Board(Config.N_ROWS, Config.N_COLUMNS);
