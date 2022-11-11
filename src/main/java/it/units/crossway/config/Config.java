074bbb8f02450f7007fd93aae50c888916408770
mcorsano <mariannacorsano98@gmail.com>
2022-11-11 01:32:02 +0100

    Cell size added

--- src/main/java/it/units/crossway/config/Config.java
+++ src/main/java/it/units/crossway/config/Config.java
@@ -2,7 +2,7 @@ package it.units.crossway.config;
 
 public class Config {
     private static final int N_ROWS = 19;
-    private static final int N_COLUMNS = 19;
+    private static final int N_COLUMNS = N_ROWS;
 
     public static final int BOARD_WIDTH = 500;
     public static final int BOARD_HEIGHT = BOARD_WIDTH;
@@ -10,11 +10,13 @@ public class Config {
     private static final int FRAME_WIDTH = BOARD_WIDTH + 20;
     private static final int FRAME_HEIGHT = BOARD_HEIGHT + 40;
 
+    private static final int CELL_SIZE = BOARD_WIDTH/N_COLUMNS;
+
+
 
     public static int getN_Rows() {
         return N_ROWS;
     }
-
     public static int getN_Columns() {
         return N_COLUMNS;
     }
@@ -24,4 +26,6 @@ public class Config {
 
     public static int getBoardWidth() { return BOARD_WIDTH; }
     public static int getBoardHeight() { return BOARD_HEIGHT; }
+
+    public static int getCellSize() { return CELL_SIZE; }
 }
