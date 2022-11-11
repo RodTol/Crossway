a572bf8f6e6c7e656118903a4318ccac484d030a
mcorsano <mariannacorsano98@gmail.com>
2022-11-11 01:20:57 +0100

    Board width and Height added

--- src/main/java/it/units/crossway/config/Config.java
+++ src/main/java/it/units/crossway/config/Config.java
@@ -4,6 +4,13 @@ public class Config {
     private static final int N_ROWS = 19;
     private static final int N_COLUMNS = 19;
 
+    public static final int BOARD_WIDTH = 500;
+    public static final int BOARD_HEIGHT = BOARD_WIDTH;
+
+    private static final int FRAME_WIDTH = BOARD_WIDTH + 20;
+    private static final int FRAME_HEIGHT = BOARD_HEIGHT + 40;
+
+
     public static int getN_Rows() {
         return N_ROWS;
     }
@@ -11,4 +18,10 @@ public class Config {
     public static int getN_Columns() {
         return N_COLUMNS;
     }
+
+    public static int getFrameWidth() { return FRAME_WIDTH; }
+    public static int getFrameHeight() { return FRAME_HEIGHT; }
+
+    public static int getBoardWidth() { return BOARD_WIDTH; }
+    public static int getBoardHeight() { return BOARD_HEIGHT; }
 }
