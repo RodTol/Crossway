ed546b927921b353295b0e17867908b460cb2bda
mcorsano <mariannacorsano98@gmail.com>
2022-11-11 11:48:48 +0100

    BoardGui size fixed

--- src/main/java/it/units/crossway/config/Config.java
+++ src/main/java/it/units/crossway/config/Config.java
@@ -3,13 +3,16 @@ package it.units.crossway.config;
 public class Config {
     public static final int N_ROWS = 19;
     public static final int N_COLUMNS = 19;
+
     public static final int BOARD_WIDTH = 500;
     public static final int BOARD_HEIGHT = BOARD_WIDTH;
+    public static final int CELL_SIZE = BOARD_WIDTH/N_COLUMNS;
+
+    public static final int BOARD_MARGIN = (BOARD_HEIGHT - (N_ROWS-1)*CELL_SIZE)/2;
 
     public static final int FRAME_WIDTH = BOARD_WIDTH + 20;
 
     public static final int FRAME_HEIGHT = BOARD_HEIGHT + 40;
 
-    public static final int CELL_SIZE = BOARD_WIDTH/N_COLUMNS;
 
 }
