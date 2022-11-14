b94b2a576942bacd7c2ded39b6ebe380b81680ce
Giulia Bernardi <giuliabernardi7791@gmail.com>
2022-11-14 23:27:54 +0100

    Added test for isNodeEmpty in Node: initialNodesAreEmpty

--- src/test/java/it/units/crossway/model/NodeTest.java
+++ src/test/java/it/units/crossway/model/NodeTest.java
@@ -1,8 +1,19 @@
 package it.units.crossway.model;
 
 import it.units.crossway.exceptions.NullPieceException;
+import it.units.crossway.utils.Config;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NodeTest {
+    @Test
+    void initialNodesAreEmpty(){
+        Board board = new Board(Config.N_ROWS,Config.N_COLUMNS);
+        for (int row = 0; row < board.getNodes().length; row++) {
+            for (int col = 0; col < board.getNodes()[row].length; col++) {
+                assertTrue(board.getNodes()[row][col].isNodeEmpty());
+            }
+        }
+    }
 }
