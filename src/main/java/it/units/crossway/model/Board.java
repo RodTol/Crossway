666e1e0065ec1dd8565c8778b0edda97f0b13b46
Giulia Bernardi <giuliabernardi7791@gmail.com>
2022-11-14 23:55:59 +0100

    Fixed nodes initialisation in Board

--- src/main/java/it/units/crossway/model/Board.java
+++ src/main/java/it/units/crossway/model/Board.java
@@ -5,15 +5,51 @@ import java.awt.*;
 
 public class Board {
 
-    /*Matrix of Pieces*/
+    /*Matrix of Nodes*/
     private Node[][] nodes;
 
-    public Board(int Nrows, int Ncolumns) {
-        this.nodes = new Node[Nrows][Ncolumns];
+    public Board(int NRows, int NColumns) {
+        this.nodes = new Node[NRows][NColumns];
+        for (int row = 0; row < nodes.length; row++) {
+            for (int col = 0; col < nodes[row].length; col++) {
+                nodes[row][col] = new Node();
+            }
+        }
     }
 
-    public boolean isNodePlayable(int r, int c) {
-        /*Controllo 8 pedine attorno per capire se giocabile*/
+    public Node[][] getNodes() {
+        return nodes;
+    }
+
+    public boolean canPlace(int r, int c, int NRows, int NColumns, Color playerColor) {
+        if (!nodes[r-1][c-1].isNodeEmpty() && nodes[r-1][c-1].getPiece().color == playerColor) {
+            if (!nodes[r-1][c].isNodeEmpty() && !nodes[r][c-1].isNodeEmpty() &&
+                    nodes[r-1][c].getPiece().color == nodes[r][c-1].getPiece().color &&
+                    nodes[r-1][c].getPiece().color != playerColor) {
+                return false;
+            }
+        }
+        if (!nodes[r-1][c+1].isNodeEmpty() && nodes[r-1][c+1].getPiece().color == playerColor) {
+            if (!nodes[r-1][c].isNodeEmpty() && !nodes[r][c+1].isNodeEmpty() &&
+                    nodes[r-1][c].getPiece().color == nodes[r][c+1].getPiece().color &&
+                    nodes[r-1][c].getPiece().color != playerColor) {
+                return false;
+            }
+        }
+        if (!nodes[r+1][c-1].isNodeEmpty() && nodes[r+1][c-1].getPiece().color == playerColor) {
+            if (!nodes[r][c-1].isNodeEmpty() && !nodes[r+1][c].isNodeEmpty() &&
+                    nodes[r][c-1].getPiece().color == nodes[r+1][c].getPiece().color &&
+                    nodes[r][c-1].getPiece().color != playerColor) {
+                return false;
+            }
+        }
+        if (!nodes[r+1][c+1].isNodeEmpty() && nodes[r+1][c+1].getPiece().color == playerColor) {
+            if (!nodes[r][c+1].isNodeEmpty() && !nodes[r+1][c].isNodeEmpty() &&
+                    nodes[r][c+1].getPiece().color == nodes[r+1][c].getPiece().color &&
+                    nodes[r][c+1].getPiece().color != playerColor) {
+                return false;
+            }
+        }
         return true;
     }
 
@@ -21,7 +57,4 @@ public class Board {
         nodes[c.getRow()][c.getColumn()].setPiece(piece);
     }
 
-    public boolean canPlace(Color color, Coordinates position) {
-        return true;
-    }
 }
