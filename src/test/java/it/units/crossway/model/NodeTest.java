12f4cb5547b3000a2213c9a7cf25835d027cdb85
Giulia Bernardi <giuliabernardi7791@gmail.com>
2022-11-15 00:09:43 +0100

    Added notEmptyNode test for isNodeEmpty in Node and fixed typo in ControllerTest

--- src/test/java/it/units/crossway/model/NodeTest.java
+++ src/test/java/it/units/crossway/model/NodeTest.java
@@ -3,17 +3,29 @@ package it.units.crossway.model;
 import it.units.crossway.exceptions.NullPieceException;
 import it.units.crossway.utils.Config;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NodeTest {
     @Test
     void initialNodesAreEmpty(){
         Board board = new Board(Config.N_ROWS,Config.N_COLUMNS);
+        //va fatto un ciclo sulle coordinate?
         for (int row = 0; row < board.getNodes().length; row++) {
             for (int col = 0; col < board.getNodes()[row].length; col++) {
                 assertTrue(board.getNodes()[row][col].isNodeEmpty());
             }
         }
+    }
+
+    @Test
+    void notEmptyNode(){
+        Board board = new Board(Config.N_ROWS,Config.N_COLUMNS);
+        Coordinates coord = new Coordinates(1,2);
+        Piece piece = new Piece(Color.BLACK);
+        board.place(coord, piece);
+        assertFalse(board.getNodes()[coord.getRow()][coord.getColumn()].isNodeEmpty());
     }
 }
