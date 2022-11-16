af7b06ca64fdde5bc43ef9e7c645e0c678f96001
Giulia Bernardi <giuliabernardi7791@gmail.com>
2022-11-16 23:11:06 +0100

    Added test Reflexivity for the Vertex class.

---
+++ src/test/java/it/units/crossway/utilities/VertexTest.java
@@ -0,0 +1,24 @@
+package it.units.crossway.utilities;
+
+import it.units.crossway.model.Piece;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class VertexTest {
+    private Vertex vertex;
+    private Piece piece;
+
+    @BeforeEach
+    public void setup(){
+        piece = new Piece(Color.BLACK);
+        vertex = new Vertex(piece);
+    }
+
+    @Test
+    void reflexivity(){
+        assertTrue(vertex==vertex);
+    }
+}
