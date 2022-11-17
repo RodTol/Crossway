72ada417cf02b17a1503ba232b13e92e201948ee
RodTol <rodolfo.tolloi@gmail.com>
2022-11-17 19:54:27 +0100

    Starting to develop a test to perform a simulation of a game

--- src/test/java/it/units/crossway/FictionalGame.java
+++ src/test/java/it/units/crossway/FictionalGame.java
@@ -4,21 +4,29 @@ import it.units.crossway.controller.Controller;
 import it.units.crossway.controller.GameController;
 import it.units.crossway.gui.BoardGui;
 import it.units.crossway.model.Board;
-import org.junit.Before;
+import it.units.crossway.model.Coordinates;
+
+import it.units.crossway.model.Piece;
+import org.junit.Assert;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.awt.*;
+
 public class FictionalGame {
 
+    private int dim = 4;
     private static Board board;
     private Controller controller;
     private BoardGui boardGui;
 
-    @Before
+    @BeforeEach
     @DisplayName("Fictional game initialisation on 4x4 board")
     public void set_up() {
-        int dim = 4;
+
         board = new Board(dim, dim);
         controller = new GameController(board);
         boardGui = Mockito.mock(BoardGui.class);
@@ -26,7 +34,12 @@ public class FictionalGame {
 
     @Test
     @DisplayName("Tests initial conditions")
-    void initial_conditions() {
-
+    public void initial_conditions() {
+        for (int r = 0; r < dim; r++) {
+            for (int c = 0; c < dim; c++) {
+                Assert.assertTrue(board.canPlace(new Coordinates(r, c), new Piece(Color.BLACK)));
+                Assert.assertNull(board.getNode(new Coordinates (r,c)).getPiece());
+            }
+        }
     }
 }
