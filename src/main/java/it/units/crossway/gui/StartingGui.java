bd610b8c41128623928c4cbc63a361200088a278
RodTol <rodolfo.tolloi@gmail.com>
2022-11-21 19:22:07 +0100

    First Try to implement an alternative Gui

--- src/main/java/it/units/crossway/gui/StartingGui.java
+++ src/main/java/it/units/crossway/gui/StartingGui.java
@@ -3,18 +3,30 @@ package it.units.crossway.gui;
 import it.units.crossway.controller.Controller;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StartingGui extends JPanel {
     private final Controller controller;
+    private JTextField white = new JTextField(20);
+    private JTextField black = new JTextField(20);
+
+    private JButton button = new JButton("Let's Play!");
 
     public StartingGui(Controller controller) {
         this.controller = controller;
+        this.add(white);
+        //this.add(black);
+        white.setHorizontalAlignment(JTextField.CENTER);
+        black.setHorizontalAlignment(JTextField.CENTER);
+
+        white.setToolTipText("WHITE PLAYER");
+        black.setToolTipText("BLACK PLAYER");
+
+        white.setFont(new java.awt.Font("Arial", Font.ITALIC | Font.BOLD, 20));
+        black.setFont(new java.awt.Font("Arial", Font.ITALIC | Font.BOLD, 20));
+
+        //button.addActionListener();
+
     }
 
-    void handleInsert(String name) {
-        /*Va gestito se inserisco il bianco o il nero*/
-        controller.setNameWhitePlayer(name);
-    }
-
-    /*Insert Gui method*/
 }
