c2afcab6988693041b796301b7ec5f56b2c525be
Giulia Bernardi <giuliabernardi7791@gmail.com>
2022-11-21 02:33:47 +0100

    Added equals and hash code for player class.

--- src/main/java/it/units/crossway/controller/Player.java
+++ src/main/java/it/units/crossway/controller/Player.java
@@ -1,5 +1,9 @@
 package it.units.crossway.controller;
 
+import it.units.crossway.model.Coordinates;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.awt.*;
 
 class Player {
@@ -8,6 +12,29 @@ class Player {
 
     public Player(Color color) {
         this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Player)) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        Player o = (Player) obj;
+        return new EqualsBuilder()
+                .append(color, o.color)
+                .append(name, o.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(color)
+                .append(name)
+                .toHashCode();
     }
 
     public Color getColor() {
