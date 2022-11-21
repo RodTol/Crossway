e774e11be0c4fce1b4f2fe49d1ad686b4cb13622
RodTol <rodolfo.tolloi@gmail.com>
2022-11-21 19:22:07 +0100

    First Try to implement an alternative Gui

--- src/main/java/it/units/crossway/launcher/Main.java
+++ src/main/java/it/units/crossway/launcher/Main.java
@@ -10,17 +10,29 @@ import it.units.crossway.gui.BoardGui;
 import javax.swing.*;
 import java.awt.*;
 
-public class    Main {
+public class Main {
+
+    private static void createWindow(String title,  int width, int height, JPanel content) {
+        JFrame frame = new JFrame(title);
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().add(content, BorderLayout.CENTER);
+        frame.setSize(new Dimension(width, height));
+        frame.setLocation(500, 200);    // location where frame appears
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
     public static void main(String[] args) {
         Board board = new Board( Config.N_ROWS, Config.N_COLUMNS);
         Controller controller = new GameController(board);
 
-        //StartingGui startingGui = new StartingGui(controller);
-        //Window start
+        StartingGui startingGui = new StartingGui(controller);
+        startingGui.setBackground(Color.LIGHT_GRAY);
+        createWindow("Player selection", 500, 500, startingGui);
 
         BoardGui boardGui = new BoardGui(controller, new BoardGuiSettings(Config.BOARD_MARGIN, Config.BOARD_HEIGHT, Config.BOARD_WIDTH, Config.CELL_SIZE));
         boardGui.setBackground(Color.LIGHT_GRAY);
-        Window game = new Window("Crossway", boardGui, Config.FRAME_WIDTH, Config.FRAME_HEIGHT);
+        createWindow("Crossway", Config.FRAME_WIDTH, Config.FRAME_HEIGHT, boardGui);
 
     }
 }
