608766c29c60abc03372e612ba91147638199a86
RodTol <rodolfo.tolloi@gmail.com>
2022-11-25 17:43:27 +0100

    Introduced demo game inside the launcher

--- src/main/java/it/units/crossway/launcher/Main.java
+++ src/main/java/it/units/crossway/launcher/Main.java
@@ -18,37 +18,43 @@ public class Main {
         MouseEvent press, release, click;
         Point point;
         long time;
+
         point = new Point(x, y);
         SwingUtilities.convertPointToScreen(point, target);
-        time    = System.currentTimeMillis();
-        press   = new MouseEvent(target, MouseEvent.MOUSE_PRESSED,  time, 0, x, y, point.x, point.y, 1, false, MouseEvent.BUTTON1);
-        release = new MouseEvent(target, MouseEvent.MOUSE_RELEASED, time, 0, x, y, point.x, point.y, 1, false, MouseEvent.BUTTON1);
-        click   = new MouseEvent(target, MouseEvent.MOUSE_CLICKED,  time, 0, x, y, point.x, point.y, 1, false, MouseEvent.BUTTON1);
-        target.dispatchEvent(press);
-        target.dispatchEvent(release);
+        time = System.currentTimeMillis();
+
+        click = new MouseEvent(target, MouseEvent.MOUSE_CLICKED, time, 0, x, y, point.x, point.y, 1, false, MouseEvent.BUTTON1);
         target.dispatchEvent(click);
-        TimeUnit.SECONDS.sleep(4);
+        TimeUnit.MILLISECONDS.sleep(200);
     }
 
-    public static void main(String[] args) {
-        Board board = new Board( Config.N_ROWS, Config.N_COLUMNS);
+
+    public static void main(String[] args) throws InterruptedException {
+        Board board = new Board(Config.N_ROWS, Config.N_COLUMNS);
         Controller controller = new GameController(board);
 
         Gui gui = new Gui(controller, new BoardPanelSettings(Config.BOARD_MARGIN,
                 Config.BOARD_HEIGHT, Config.BOARD_WIDTH, Config.CELL_SIZE));
 
 
-        int[] x_moves = new int[] {120, 140, 160};
-        int[] y_moves = new int[] {120,140, 160};
-
-        for (int i = 0; i < x_moves.length; i++) {
+        while (gui.getCurrentPanel() == 1) {
             try {
-                click(gui.getBoardPanel(), x_moves[i], y_moves[i]);
+                Thread.sleep(3000);
+                //Any other code to execute after 5 min execution pause.
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
 
-
+        /*Delta pixel = 19, (0,0) = (16,16)*/
+       System.out.println("Robot starts playing");
+        for (int i = 0; i < 19; i++) {
+            click(gui.getBoardPanel(), 16+26*4, 16 + i*26);
+            click(gui.getBoardPanel(), 16+26*5, 16 + i*26 );
+        }
     }
 }
+
+
+
+
