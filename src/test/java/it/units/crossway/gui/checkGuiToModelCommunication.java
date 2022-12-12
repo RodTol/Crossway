3c678ae7211282fc5b2f223619fc8be89eaf57a4
RodTol <rodolfo.tolloi@gmail.com>
2022-12-12 19:28:35 +0100

    Refactor of a test

--- src/test/java/it/units/crossway/gui/checkGuiToModelCommunication.java
+++ src/test/java/it/units/crossway/gui/checkGuiToModelCommunication.java
@@ -4,27 +4,31 @@ import it.units.crossway.controller.Controller;
 import it.units.crossway.controller.GameController;
 import it.units.crossway.model.Board;
 import it.units.crossway.model.Coordinates;
+import it.units.crossway.model.Piece;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
 public class checkGuiToModelCommunication {
 
-    private final int dim = 19 ;
+    private final static int dim = 5 ;
     private final Board board = new Board(dim, dim);
     private final Controller controller = new GameController(board);
-    private final BoardPanel boardPanel = new BoardPanel(controller, new BoardPanelSettings(300, 300, 300, 300));
+    private final BoardPanel boardPanel = new BoardPanel(controller, new BoardPanelSettings(50, 300, 300, 300/5));
 
     @BeforeEach
     void setUp() {
         controller.setNameWhitePlayer("test1");
         controller.setNameBlackPlayer("test2");
-        boardPanel.drawNames();
+        boardPanel.drawNamesLabel();
     }
 
     @Test
@@ -37,4 +41,24 @@ public class checkGuiToModelCommunication {
         assertFalse(controller.getBoard().getNode(test_coordinates).isNodeEmpty());
     }
 
+    @Test
+    @DisplayName("Ghost display only in correct position")
+    void ghostInIllegalPosition() {
+        int[] rows = new int[]{1, 2, 2, 1};
+        int[] cols = new int[]{1, 1, 2, 2};
+
+        for (int i=0; i<3; i++) {
+            boardPanel.handleMouseClicked(boardPanel.nodePositionToPx(new Coordinates(rows[i], cols[i])));
+        }
+
+        assertFalse(board.canPlace(new Coordinates(rows[3],cols[3]), new Piece(Color.WHITE) ));
+
+        Point ghostPositionBefore = boardPanel.getGhostPosition();
+        boardPanel.handleMouseMoved(boardPanel.nodePositionToPx(new Coordinates(rows[3], cols[3])));
+
+        assertEquals(ghostPositionBefore, boardPanel.getGhostPosition());
+
+        boardPanel.handleMouseMoved(boardPanel.nodePositionToPx(new Coordinates(rows[3]+1, cols[3]+1)));
+        assertNotEquals(ghostPositionBefore, boardPanel.getGhostPosition());
+    }
 }
